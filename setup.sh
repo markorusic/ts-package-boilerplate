ba03057657d9938bae1ae6a4f6d65b37dcb222b9
Marko Rusic <markousp5@gmail.com>
2019-08-22 23:17:38 +0200

    updated setup script

--- setup.sh
+++ setup.sh
@@ -7,11 +7,16 @@ if [ $# -eq 0 ]
     exit 1
 fi
 
+rm -rf .git
 rm -rf node_modules
 rm -rf dist
 rm -rf coverage
 
 grep -rl "ts-package-boilerplate" . | xargs sed -i "s/ts-package-boilerplate/$1/g"
+
+git init
+git commit -m "init commit"
+git config credential.helper store
 
 mv ../ts-package-boilerplate ../$1
 
